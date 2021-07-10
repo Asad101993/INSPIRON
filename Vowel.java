8b5716972abbd72e153f2b9d4dc6026cb93adf7f
Asad101993 <86704544+Asad101993@users.noreply.github.com>
2021-07-10 19:19:18 +0530

    Delete Vowel.java

--- Vowel.java
+++
@@ -1,9 +0,0 @@
-public class Vowel {
-		public static void main(String[] args) {
-		char ch = 'i';
-		if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
-		System.out.println(ch + " is vowel");
-		else
-		System.out.println(ch + " is consonant");
-		}
-		}
